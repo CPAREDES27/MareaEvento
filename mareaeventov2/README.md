a6b86083de943544e1658703be72b4861517b7b9
Alejandro Magno <85909530+amagno962021@users.noreply.github.com>
2022-02-09 16:51:35 -0500

    Add files via upload

---
+++ mareaeventov2/README.md
@@ -0,0 +1,36 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Wed Feb 09 2022 12:03:11 GMT-0500 (hora estándar de Perú)|
+|**App Generator**<br>@sap/generator-fiori|
+|**App Generator Version**<br>1.4.4|
+|**Generation Platform**<br>Visual Studio Code|
+|**Floorplan Used**<br>simple|
+|**Service Type**<br>None|
+|**Service URL**<br>N/A
+|**Module Name**<br>mareaeventov2|
+|**Application Title**<br>Detalle Marea|
+|**Namespace**<br>com.tasa|
+|**UI5 Theme**<br>sap_fiori_3|
+|**UI5 Version**<br>Latest|
+|**Enable Code Assist Libraries**<br>False|
+|**Add Eslint configuration**<br>False|
+|**Enable Telemetry**<br>True|
+
+## mareaeventov2
+
+A Fiori application.
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply run the following from the generated app root folder:
+
+```
+    npm start
+```
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ mareaeventov2/webapp/Component.js
@@ -0,0 +1,79 @@
+sap.ui.define([
+        "sap/ui/core/UIComponent",
+        "sap/ui/Device",
+        "com/tasa/mareaeventov2/model/models"
+    ],
+    function (UIComponent, Device, models) {
+        "use strict";
+
+        return UIComponent.extend("com.tasa.mareaeventov2.Component", {
+            metadata: {
+                manifest: "json"
+            },
+
+            /**
+             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+             * @public
+             * @override
+             */
+            init: function () {
+                // call the base component's init function
+                UIComponent.prototype.init.apply(this, arguments);
+
+                // enable routing
+                this.getRouter().initialize();
+
+                // set the device model
+                this.setModel(models.createDeviceModel(), "device");
+
+                // set detalle marea model
+                this.setModel(models.createInitModel(), "DetalleMarea");
+
+                //set combos model
+                this.setModel(models.createCombosModel(), "ComboModel");
+
+                //sett plantas model
+                this.setModel(models.createPlantasModel(), "PlantasModel");
+
+                //set propios model
+                this.setModel(models.ListaMareas(), "ListaMareas");
+
+                //set constants utilitis model
+                this.setModel(models.createConstantsUtility(), "ConstantsUtility");
+
+                //set form model
+                sap.ui.getCore().setModel(models.createFormModel(), "Form");
+
+                //set data session model
+                sap.ui.getCore().setModel(models.createDataSession(), "DataSession");
+
+                //set visible model
+                sap.ui.getCore().setModel(models.createVisibleModel(), "Visible");
+
+                //set resource model i18n
+                sap.ui.getCore().setModel(models.createResourceModel(), "i18n");
+
+                //set Utils model
+                sap.ui.getCore().setModel(models.createUtilsModel(), "Utils");
+
+                //set marea anterior model
+                sap.ui.getCore().setModel(models.createMareaAnteriorModel(), "MareaAnterior");
+
+                //set utilitario model
+                sap.ui.getCore().setModel(models.createUttilitarioModel(), "Utilitario");
+
+                //set filtro model
+                sap.ui.getCore().setModel(models.createFiltroModel(), "Filtro");
+
+                //set Distrib Flota model
+                sap.ui.getCore().setModel(models.createDistribFlotaModel(), "DistribFlota");
+
+                //set constants utiliy model
+                //sap.ui.getCore().setModel(models.createConstantsUtility(), "ConstantsUtility");
+
+                //set constantes model
+                sap.ui.getCore().setModel(models.createConstantesModel(), "Constantes");
+            }
+        });
+    }
+);
